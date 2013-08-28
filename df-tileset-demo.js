8b3097ef4e325aae7f49b5f085b2ddad56e6d3c8
lethosor <stahlalan19@gmail.com>
2013-08-28 09:12:38 -0400

    Improve demo: Color messages

--- df-tileset-demo.js
+++ df-tileset-demo.js
@@ -11,11 +11,17 @@ Demo.init = function(){
 		'overflow-y': 'scroll',
 		'white-space': 'pre'
 	}).appendTo('body').html('<div></div>');
-	Demo.log('Demo initialized.');
+	Demo.log('{green|Demo initialized}.');
 }
 
 Demo.logf = function(){
-	$('#demo-log div').html($('#demo-log div').html() + [].slice.apply(arguments).join(' '));
+	var txt = [].slice.apply(arguments).join(' ');
+	txt = txt.replace(/\{([^|]+)\|([^|}]+)\}/g, function(a, c, t){
+		return '<span style="color:'+c+'">'+t+'</span>';
+	}).replace(/\{([^|]+)\|([^|]+)\|([^|}]+)\}/g, function(a, c, b, t){
+		return '<span style="color:'+c+'; background-color:'+b+'">'+t+'</span>';
+	});
+	$('#demo-log div').html($('#demo-log div').html() + txt);
 	$('#demo-log').scrollTop($('#demo-log').children().height());
 };
 Demo.log = function(){
@@ -30,25 +36,28 @@ $(function(){
 		var time, otime;
 		otime = time = (new Date()).getTime();
 		font = Tileset.Font('#tileset');
+		var font_bg = 'rgba(' + font.characters[0].pixels[0][0].toString() + ')';
 		Demo.log('Font loaded in ' + ((new Date()).getTime() - time) + ' ms.\nFont:',
 				 font.characters.length, 'characters, background:',
-				 'rgba(' + font.characters[0].pixels[0][0].toString() + ')');
+				 '{white|' + font_bg + '|' + font_bg + '}');
 		var canvas = $('<canvas>').prependTo('body').attr({
 			width: $('body').width(),
 			height: $('body').height() / 2
 			}),
 			cx = canvas[0].getContext('2d');
 		Demo.log('Multiple color/caching demo (5 times)')
+		ch = Math.floor(Math.random() * 256);
+		Demo.log('Using character', ch);
 		for (var i = 1; i <= 5; i++) {
 			time = (new Date()).getTime();
 			Demo.logf('#' + i + ': ');
 			for (var r = 0; r < 256; r += 5) {
 				for (var g = 0; g < 256; g += 15) {
-					var d = font.characters[1].image_data([255,255,255], [r,g,0]);
+					var d = font.characters[ch].image_data([255,255,255], [r,g,0]);
 					cx.putImageData(d, r/5*d.width, g/15*d.height);
 				}
 			}
-			Demo.log('Done ('+ ((new Date()).getTime() - time), 'ms)');
+			Demo.log('{green|Done} ('+ ((new Date()).getTime() - time), 'ms)');
 		}
 		
 		Demo.log('Total time for all demos:', (new Date()).getTime() - otime, 'ms');
